d6e809a7d0b712a1f4a0c0bc4d708c459712f44b
zhony94 <zhony94@gmail.com>
2022-05-23 09:40:59 +0600

    feat: 04-copy-directory task solved

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,36 @@
+const fs = require('fs');
+const path = require('path');
+
+const folderPath = path.join(__dirname, 'files');
+const copyFolderPath = path.join(__dirname, 'files-copy');
+
+fs.stat(copyFolderPath, function(err) {
+    if (!err) {
+    console.log('files-copy directory exists\ncopying missing files');
+    }
+    else if (err.code === 'ENOENT') {
+    console.log('files-copy does not exist\nfolder is created\ncopying files');
+    fs.mkdir(copyFolderPath, (err) => {
+        if(err){
+            return console.error(err)
+        }
+    })
+    }
+});
+
+fs.readdir(folderPath, {withFileTypes:true}, function(err, items){
+    items.forEach(element => {
+        if(element.isFile()){
+            const filePath = path.join(folderPath, element.name)
+            const copyFilePath = path.join(copyFolderPath, element.name)
+
+            fs.copyFile(filePath, copyFilePath, function(err){
+                if(err){
+                    return console.error(err)
+                }
+            })   
+        }  
+    });
+    console.log('all files have been copied succesfully')
+})
 
